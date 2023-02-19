abd28d89914154cda717ffdc46888d2171b96272
Amr Attia <amr.atyia.ham@gmail.com>
2023-02-19 20:38:31 +0900

    added test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React, { AnchorHTMLAttributes } from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByLabelText("learn-link");
+  expect(linkElement).toHaveClass("App-link");
+  expect(linkElement).toHaveTextContent("Learn React with me");
   expect(linkElement).toBeInTheDocument();
+});
+
+test("url is correct", () => {
+  render(<App />);
+  const linkElement = screen.getByLabelText("learn-link");
+  expect((linkElement as HTMLAnchorElement).href).toEqual(
+    "https://reactjs.org/"
+  );
 });
